4caf2d9b097349817a137e4097d5be11773aa632
klemavilanova <klemavilanova@victoria18.danielcastelao.org>
2016-12-12 09:44:17 +0100

    extraccion del metodo calcular factorial

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -2,16 +2,19 @@ package factorial;
 
 public class Factorial {
 
-    public static void main(String[] args) {
-
-        int f = 1;
-        int j = 8;
-
+    public static long calcFactorial(int f, int j) {
         for (int i = j; i >= 1; i--) {
             f = f * i;
         }
+        return f;
+    }
 
-        System.out.println(f);
+    public static void main(String[] args) {
+
+        int factorial = 1;
+        int j = 8;
+        
+        System.out.println(Factorial.calcFactorial(factorial,j));
 
     }
 
